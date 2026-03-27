edabdbc0dbfa1f8b988e45332b55ff8787a36e7e
Govind jangid <govindkumarjangid17@gmail.com>
2026-03-27 08:07:14 +0530

    fix scroll bar issue and url conflict

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: 'https://carrental-nezp.onrender.com',
+    baseURL: 'https://carrental-nezp.onrender.com/',
+    // baseURL:'http://localhost:8080/',
     withCredentials: true,
 });
