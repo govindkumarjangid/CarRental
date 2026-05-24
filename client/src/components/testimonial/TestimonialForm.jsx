759712b28bfc7cc298c5db633ecf30fe7053ea0e
Govind jangid <govindkumarjangid17@gmail.com>
2026-05-24 15:41:13 +0530

    fix testimonial form opening and closing issue

--- client/src/components/testimonial/TestimonialForm.jsx
+++ client/src/components/testimonial/TestimonialForm.jsx
@@ -53,134 +53,135 @@ const TestimonialForm = () => {
 				transition={{ duration: 0.3 }}
 				onClick={() => setShowReview(false)}
 				className="fixed inset-0 z-100 backdrop-blur-xs flex items-center justify-center bg-blue-700/5 p-4"
-			/>
-			<motion.form
-				onSubmit={handleSubmit}
-				onClick={(e) => e.stopPropagation()}
-				initial={{ opacity: 0, filter: "blur(5px)", scale: 0.95 }}
-				animate={{ opacity: 1, filter: "blur(0px)", scale: 1 }}
-				exit={{ opacity: 0, filter: "blur(5px)", scale: 0.95 }}
-				transition={{
-					delay: 0.1,
-					duration: 0.3,
-					ease: "easeOut"
-				}}
-				key="testimonial-form"
-				className="bg-white md:rounded-md w-full h-full md:h-fit max-w-2xl md:max-h-[90vh] overflow-y-auto shadow-2xl relative p-4 md:p-6"
 			>
-				<div className="flex items-center justify-between mb-4">
-					<h2 className="text-xl font-semibold"
-					>
-						Add Your Review
-					</h2>
-					<button
-						type="button"
-						onClick={() => setShowReview(false)}
-						className="bg-gray-100 p-1 h-8 w-8 rounded-md text-gray-600 hover:text-gray-700 cursor-pointer active:scale-95 transition-transform duration-200"
-					>
-						<iconList.X size={25} />
-					</button>
-				</div>
-
-				{/* photo */}
-				<div
-					className="flex gap-4 items-center w-full mb-6"
+				<motion.form
+					onSubmit={handleSubmit}
+					onClick={(e) => e.stopPropagation()}
+					initial={{ opacity: 0, filter: "blur(5px)", scale: 0.95 }}
+					animate={{ opacity: 1, filter: "blur(0px)", scale: 1 }}
+					exit={{ opacity: 0, filter: "blur(5px)", scale: 0.95 }}
+					transition={{
+						delay: 0.1,
+						duration: 0.3,
+						ease: "easeOut"
+					}}
+					key="testimonial-form"
+					className="bg-white md:rounded-md w-full h-full md:h-fit max-w-2xl md:max-h-[90vh] overflow-y-auto shadow-2xl relative p-4 md:p-6"
 				>
-					<label htmlFor="car-image">
-						{image ? (
-							<img
-								src={URL.createObjectURL(image)}
-								alt="car preview"
-								className="h-12 w-20 object-cover rounded-md"
+					<div className="flex items-center justify-between mb-4">
+						<h2 className="text-xl font-semibold"
+						>
+							Add Your Review
+						</h2>
+						<button
+							type="button"
+							onClick={() => setShowReview(false)}
+							className="bg-gray-100 p-1 h-8 w-8 rounded-md text-gray-600 hover:text-gray-700 cursor-pointer active:scale-95 transition-transform duration-200"
+						>
+							<iconList.X size={25} />
+						</button>
+					</div>
+
+					{/* photo */}
+					<div
+						className="flex gap-4 items-center w-full mb-6"
+					>
+						<label htmlFor="car-image">
+							{image ? (
+								<img
+									src={URL.createObjectURL(image)}
+									alt="car preview"
+									className="h-12 w-20 object-cover rounded-md"
+								/>
+							) : (
+								<iconList.CloudUpload className="h-14 text-primary bg-gray-100 px-4 py-3 rounded-md cursor-pointer w-26 border border-gray-200" />
+							)}
+
+							<input
+								type="file"
+								id="car-image"
+								name="car-image"
+								accept="image/*"
+								hidden
+								onChange={(e) => setImage(e.target.files[0])}
 							/>
-						) : (
-							<iconList.CloudUpload className="h-14 text-primary bg-gray-100 px-4 py-3 rounded-md cursor-pointer w-26 border border-gray-200" />
-						)}
+						</label>
+						<p className="text-sm">Upload your image</p>
+					</div>
 
-						<input
-							type="file"
-							id="car-image"
-							name="car-image"
-							accept="image/*"
-							hidden
-							onChange={(e) => setImage(e.target.files[0])}
+					{/* name & email  */}
+					<div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+						<InputBox
+							label="name"
+							title="Name"
+							value={form.name}
+							onChange={handleChange}
+							placeholder="Full Name"
 						/>
-					</label>
-					<p className="text-sm">Upload your image</p>
-				</div>
+						<InputBox
+							label="email"
+							title="Email"
+							type="email"
+							value={form.email}
+							onChange={handleChange}
+							placeholder="Email Address"
+						/>
+					</div>
 
-				{/* name & email  */}
-				<div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
-					<InputBox
-						label="name"
-						title="Name"
-						value={form.name}
-						onChange={handleChange}
-						placeholder="Full Name"
-					/>
-					<InputBox
-						label="email"
-						title="Email"
-						type="email"
-						value={form.email}
-						onChange={handleChange}
-						placeholder="Email Address"
-					/>
-				</div>
+					<div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+						<InputBox
+							label="location"
+							title="Location"
+							value={form.location}
+							onChange={handleChange}
+							placeholder="City, Country"
+						/>
+						<InputBox
+							label="rating"
+							title="Rating"
+							as="select"
+							value={form.rating}
+							onChange={handleChange}
+							placeholder="Select rating"
+							options={[
+								{ value: "1", label: "⭐ 1" },
+								{ value: "2", label: "⭐ 2" },
+								{ value: "3", label: "⭐ 3" },
+								{ value: "4", label: "⭐ 4" },
+								{ value: "5", label: "⭐ 5" },
+							]}
+						/>
+					</div>
 
-				<div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
 					<InputBox
-						label="location"
-						title="Location"
-						value={form.location}
+						label="review"
+						title="Review"
+						as="textarea"
+						value={form.review}
 						onChange={handleChange}
-						placeholder="City, Country"
+						rows={4}
+						placeholder="Write your experience..."
 					/>
-					<InputBox
-						label="rating"
-						title="Rating"
-						as="select"
-						value={form.rating}
-						onChange={handleChange}
-						placeholder="Select rating"
-						options={[
-							{ value: "1", label: "⭐ 1" },
-							{ value: "2", label: "⭐ 2" },
-							{ value: "3", label: "⭐ 3" },
-							{ value: "4", label: "⭐ 4" },
-							{ value: "5", label: "⭐ 5" },
-						]}
-					/>
-				</div>
 
-				<InputBox
-					label="review"
-					title="Review"
-					as="textarea"
-					value={form.review}
-					onChange={handleChange}
-					rows={4}
-					placeholder="Write your experience..."
-				/>
-
-				<button
-					type="submit"
-					className="flex items-center justify-center gap-2 sm:px-6 sm:py-2.5 py-2 px-4 bg-primary text-white rounded-md w-max mt-4 mb-2 hover:bg-primary/90 active:scale-95 transition cursor-pointer md:text-base text-sm font-semibold"
-					disabled={loading}
-				>
-					{loading ? (
-						<>
-							<iconList.Loader
-								size={10}
-								className="h-5 w-5 animate-spin text-white"
-							/>{" "}
-							Submitting...
-						</>
-					) : (
-						<>Submit Review</>
-					)}
-				</button>
-			</motion.form>
+					<button
+						type="submit"
+						className="flex items-center justify-center gap-2 sm:px-6 sm:py-2.5 py-2 px-4 bg-primary text-white rounded-md w-max mt-4 mb-2 hover:bg-primary/90 active:scale-95 transition cursor-pointer md:text-base text-sm font-semibold"
+						disabled={loading}
+					>
+						{loading ? (
+							<>
+								<iconList.Loader
+									size={10}
+									className="h-5 w-5 animate-spin text-white"
+								/>{" "}
+								Submitting...
+							</>
+						) : (
+							<>Submit Review</>
+						)}
+					</button>
+				</motion.form>
+			</motion.div>
 		</>
 
 	)
